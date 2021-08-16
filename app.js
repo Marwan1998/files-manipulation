c28a2d4778813f5bed7cdfc7ec7ba7273dd0c0ef
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-08-17 01:09:43 +0200

    added some features

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ fs.readdir(pathName, (err, files) => {
   if (!err) {
     let counter = 1;
     files.forEach((file, index) => {
-      let newFileNm = pathName + newName(file);
+      let newFileNm = pathName + newName(file); // Only Edite this function params
       fs.rename(pathName + file, newFileNm, (er) => {
         if (!er) {
           console.log(counter + ' files renamed successful');
@@ -24,12 +24,15 @@ fs.readdir(pathName, (err, files) => {
   }
 });
 
-function newName(name, rep = '') {
+function newName(name, rep='', withThis='', def=true) {
     let newN;
+    if(def){
     newN = name.replace(' - YouTube', '')
     .replace(' - YouTube_2', '')
     .replace('(480P)_1', '')
+    .replace('(480P)', '')
     .replace('(720P_HD)_1', '')
-    .replace(rep, '');
+    }
+    newN = def ? newN.replace(rep, withThis) : name.replace(rep, withThis);
     return newN;
 }
